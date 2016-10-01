73a798b8530ffd49790091d0f772e9715645f762
dawsonrichey89 <me@dawsonrichey.com>
2016-10-01 18:24:32 -0400

    Update mad_lib.js

--- mad_lib.js
+++ mad_lib.js
@@ -5,6 +5,6 @@ var answer3 = prompt("Noun pl");
 var answer4 = prompt("Verb");
 var answer5 = prompt("Verb");
 
-var story = "There was a/an " + answer1 + " woman who lived in a/an " + answer2 + "She had so many " + answer3 + " she did know what to do so She gave them some broth without any " + answer4 + ". " + answer5 + " them all soundly and put them to bed. ";
+var story = "<p> There was a/an " + answer1 + " woman who lived in a/an " + answer2 + "She had so many " + answer3 + " she did know what to do so She gave them some broth without any " + answer4 + ". " + answer5 + " them all soundly and put them to bed. </p>";
 
 document.write(story);
