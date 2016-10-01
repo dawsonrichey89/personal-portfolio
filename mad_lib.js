8cf6cdcf7bc2790a4c5b78645db0e67a3e7a501c
dawsonrichey89 <me@dawsonrichey.com>
2016-10-01 18:15:25 -0400

    Update mad_lib.js

--- mad_lib.js
+++ mad_lib.js
@@ -1,5 +1,8 @@
 // Note
 
-var story = "This is the story";
+var story = "There was a/an " + answer1 + " woman who lived in a/an " + answer2 + ". 
+She had so many " + answer3 + " she didn't know what to do. 
+She gave them some broth without any " + answer4 + ". 
+She " + answer5 + " them all soundly and put them to bed. ";
 
 document.write(story);
