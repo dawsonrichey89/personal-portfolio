54b5a6361a1b272f25e05d199716873a6b7e752d
dawsonrichey89 <me@dawsonrichey.com>
2016-10-01 18:34:42 -0400

    Update mad_lib.js

--- mad_lib.js
+++ mad_lib.js
@@ -1,10 +1,10 @@
 // Note
 var answer1 = prompt("Adjective");
 var answer2 = prompt("Noun");
-var answer3 = prompt("Noun pl");
-var answer4 = prompt("Verb");
-var answer5 = prompt("Verb");
+var answer3 = prompt("Noun plural");
+var answer4 = prompt("Noun");
+var answer5 = prompt("Verb ending in 'ed'");
 
-var story = "<p> There was a/an " + answer1 + " woman who lived in a/an " + answer2 + "She had so many " + answer3 + " she did know what to do so She gave them some broth without any " + answer4 + ". " + answer5 + " them all soundly and put them to bed. </p>";
+var madlib = "<p> There was a/an " + answer1 + " woman who lived in a/an " + answer2 + ". <br>She had so many " + answer3 + " she didn't know what to do.<br>She gave them some broth without any " + answer4 + ".<br>She " + answer5 + " them all soundly and put them to bed. </p>";
 
-document.write(story);
+document.write(madlib);
