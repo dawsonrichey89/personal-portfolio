ca2d3a22146dfd3394642f638fc18b42eda9a7ca
dawsonrichey89 <me@dawsonrichey.com>
2016-10-01 18:15:50 -0400

    Update mad_lib.js

--- mad_lib.js
+++ mad_lib.js
@@ -1,6 +1,6 @@
 // Note
 
-var story = "There was a/an " + answer1 + " woman who lived in a/an " + answer2 + ". 
+var story = "There was a/an " + answer1 + " woman who lived in a/an " + answer2 + " . 
 She had so many " + answer3 + " she didn't know what to do. 
 She gave them some broth without any " + answer4 + ". 
 She " + answer5 + " them all soundly and put them to bed. ";
